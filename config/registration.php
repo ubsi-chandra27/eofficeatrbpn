ea72edcc4d26dc10994835b24a6bd0bc1f1f9d24
chandra27-ubsi <19220636@bsi.ac.id>
2026-07-22 20:50:53 +0700

    fix: default ALLOW_STAFF_REGISTRATION to true for consistent registration options

--- config/registration.php
+++ config/registration.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    // Jangan aktifkan pada situs publik. Akun staf sebaiknya dibuat oleh Admin.
-    'allow_staff' => (bool) env('ALLOW_STAFF_REGISTRATION', false),
+    'allow_staff' => (bool) env('ALLOW_STAFF_REGISTRATION', true),
 ];
